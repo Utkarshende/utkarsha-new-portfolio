f23b8eaad6370e44e5a36353300d629c63208f52
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-02-14 20:45:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-blue-500 underline">
+        Tailwind v4 is Live!
+      </h1>
+    </div>
+  )
+}
+export default App
