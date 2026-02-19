6a27ec56b9f0b125c6aa25edb4032ff1b5afc5de
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-02-19 21:11:06 +0530

    added mern certificate in certificate.js

--- src/configs/Certifications.js
+++ src/configs/Certifications.js
@@ -1,8 +1,11 @@
+import mernCertificate from "../assets/certifications/mern-certificate.png";
+
 const CERTIFICATIONS = [
   {
-    title: "  Full Stack Development (MERN Stack)",
+    title: "Full Stack Development (MERN Stack)",
     duration: "January 2025 - September 2025",
-    to: "https://drive.google.com/file/d/1A-MQ98k9gA2XP31eFcXFbYjPhYOpRpT1/view",
+    image: mernCertificate,
   },
 ];
+
 export default CERTIFICATIONS;
